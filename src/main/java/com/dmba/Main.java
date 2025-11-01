1702fa28e1fea44fe865fb4eba953a6913c7503a
Dima <workday2006@gmail.com>
2025-11-01 18:58:27 +0200

    init

--- src/main/java/com/dmba/Main.java
+++ src/main/java/com/dmba/Main.java
@@ -1,7 +1,41 @@
 package com.dmba;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+    }
+}
+
+class MyOwnEncoder {
+    public static final String ALPHABET = "qwertyuiopQWERTYUIOP123456789";
+
+    public static final Integer ALPHABET_SIZE = ALPHABET.length();
+
+    public static final Integer LENGTH_OF_SHORT = 6;
+
+    public String getShort(String longAddress) {
+        for(String s: longAddress) {
+
+        }
+    }
+
+    public String getLong(String shortAddress) {
+
+    }
+}
+
+class UrlShorterService {
+
+    public MyOwnEncoder encoder;
+
+    public ConcurrentMap<String, String> shorterMap = new ConcurrentHashMap<>();
+
+    public UrlShorterService(MyOwnEncoder encoder) {
+        this.encoder = encoder;
     }
 }
