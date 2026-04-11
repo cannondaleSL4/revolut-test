4483ce266ceb8d562dfd8504481d871fbe48d53b
dmba <workday2006@gmail.com>
2026-04-11 20:52:04 +0300

    init commit

--- src/test/java/com/dmba/CashbackProcessorTest.java
+++ src/test/java/com/dmba/CashbackProcessorTest.java
@@ -1,5 +1,6 @@
 package com.dmba;
 
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,23 +9,25 @@ import org.mockito.Mockito;
 
 import java.math.BigDecimal;
 import java.util.UUID;
-import java.util.concurrent.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-class CashbackProcessorTest {
+class CashBackProcessorTest {
 
-    private CashbackProcessor spyProcessor;
-    private ExecutorService executorService;
-    private final int threads = 10;
-    private final int iterations = 50;
+   private CashBackProcessor spyProcessor;
+   private ExecutorService executorService;
+   private final int threads = 10;
+   private final int iterations = 50;
 
     @BeforeEach
     void setUp() {
-        spyProcessor = Mockito.spy(new CashbackProcessor());
+        spyProcessor = Mockito.spy(new CashBackProcessor());
         executorService = Executors.newFixedThreadPool(threads);
     }
 
@@ -34,24 +37,24 @@ class CashbackProcessorTest {
     }
 
     @Test
-    void shouldHandleStabilityAndLimitsUnderConcurrency() throws InterruptedException {
-        UUID userId = UUID.randomUUID();
-        UUID duplicateTxId = UUID.randomUUID();
+    void onTransactionReceived() throws InterruptedException {
 
         CountDownLatch startLatch = new CountDownLatch(1);
-        CountDownLatch endLatch = new CountDownLatch(iterations);
+        CountDownLatch endLatch = new CountDownLatch(threads);
+        UUID userId = UUID.randomUUID();
+        UUID duplicateId = UUID.randomUUID();
 
-        for (int i = 0; i < iterations; i++) {
+        for (int i=0 ; i < iterations; i++) {
             executorService.submit(() -> {
                 try {
                     startLatch.await();
 
                     spyProcessor.onTransactionReceived(
-                        new TransactionEvent(duplicateTxId, userId, new BigDecimal("10.00"), "5411")
+                            new TransactionEvent(duplicateId, userId, new BigDecimal("10.00"), "5411")
                     );
 
                     spyProcessor.onTransactionReceived(
-                        new TransactionEvent(UUID.randomUUID(), userId, new BigDecimal("100000.00"), "5411")
+                            new TransactionEvent(UUID.randomUUID(), userId, new BigDecimal("100000.00"), "5411")
                     );
 
                 } catch (InterruptedException e) {
@@ -65,8 +68,7 @@ class CashbackProcessorTest {
         startLatch.countDown();
 
         boolean finished = endLatch.await(5, TimeUnit.SECONDS);
-        assertTrue(finished, "Threads did not complete in time");
-
+        assertTrue(finished);
         ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class);
         verify(spyProcessor, atLeastOnce()).saveToDb(eq(userId), captor.capture());
 
